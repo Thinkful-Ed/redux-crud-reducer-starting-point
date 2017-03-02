67eef80a324d05ebb6b8c52047fd32397c90155b
Joe Turner <joe@oampo.co.uk>
2017-03-02 13:34:02 +0000

    Add starting point.

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -6,21 +6,10 @@ const initialState = {
 
 export const crudReducer = (state=initialState, action) => {
     if (action.type === ADD_ITEM) {
-        return Object.assign({}, state, {
-            items: [...state.items, action.item]
-        });
     }
     else if (action.type === UPDATE_ITEM) {
-        return Object.assign({}, state, {
-            items: state.items.map(item =>
-                item.id === action.item.id ? action.item : item
-            )
-        });
     }
     else if (action.type === DELETE_ITEM) {
-        return Object.assign({}, state, {
-            items: state.items.filter(item => item.id !== action.item.id)
-        });
     }
     return state;
 };
